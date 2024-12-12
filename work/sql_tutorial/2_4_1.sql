ffc08a22f383d167e2fa7f48bad2f699c095f75b
Saki <ono.s.831@ms.saitama-ac.jp>
2024-12-12 11:07:18 +0900

    [2024/12/12 22TI083 22TI083:大野咲希] チュートリアル課題提出

--- work/sql_tutorial/2_4_1.sql
+++ work/sql_tutorial/2_4_1.sql
@@ -1,10 +1,1 @@
-mydb=# INSERT INTO weather VALUES ('San Francisco', 46, 50, 0.25, '1994-11-27');
-INSERT 0 1
-mydb=# INSERT INTO cities VALUES ('San Francisco', '(-194.0, 53.0)');
-INSERT 0 1
-mydb=# INSERT INTO weather (city, temp_lo, temp_hi, prcp, date)
-    VALUES ('San Francisco', 43, 57, 0.0, '1994-11-29');
-INSERT 0 1
-mydb=# INSERT INTO weather (date, city, temp_hi, temp_lo)
-    VALUES ('1994-11-29', 'Hayward', 54, 37);
-INSERT 0 1
+INSERT INTO weather VALUES ('San Francisco', 46, 50, 0.25, '1994-11-27');
